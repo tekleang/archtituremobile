57b569715e379b97ce639c5304626b81b4027412
tekleang <tekleangtomnerb@gmail.com>
2019-01-16 11:52:05 +0700

    first commit

---
+++ footer-mobile.php
@@ -0,0 +1,210 @@
+		<!-- footer -->
+	<div id="wrapper">
+		<div id="footer">
+			<div class="container-footer">
+				<!-- Business Partners And Clients -->
+				<div class="partner">
+					<div class="group-partner" style="padding-top: 50px;padding-bottom: 50px;">
+						<h2>OUR TRUSTED PARTNERS</h2>
+						<div class="text-align-center">
+								<div class="slick-partner">
+									<div class="item-center">
+										<div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-01.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-02.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-03.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-04.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-05.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-06.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-07.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-08.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-09.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-010.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-011.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+											 </div>
+											 
+										 	
+										 	
+										 </div>
+									</div>
+									 
+									<div class="item-center">
+										<div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-01.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-02.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-03.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-04.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-05.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-06.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-07.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-08.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-09.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-010.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-011.png" alt="">
+										 		</div>
+										 	</div>
+										 	<div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+											 </div>
+											 <div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+											 </div>
+											 <div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+											 </div>
+											 <div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+											 </div>
+											 <div class="sub-box-client">
+										 		<div class="client item-center">
+										 			<img src="<?php echo $BATH_URL;?>assets/images/KhmerAdsZone-Client-012.png" alt="">
+										 		</div>
+										 	</div>
+										 	
+										 	
+										 </div>
+									</div>
+
+								</div>
+						</div>
+
+					</div>
+				</div>
+
+<style type="text/css">
+	#footer .footer{text-align: center;}
+	#footer .footer .group-footer{text-align: center;padding-top: 50px;}
+	#footer .footer .group-footer h2{font-size: 18px;text-transform: uppercase;text-align: center;}
+	#footer .footer .group-footer a{color: black;text-decoration: none;}
+	#footer .footer .information{margin-top: 20px;}
+	#footer .footer .address{margin-top: 20px;}
+	#footer .footer .address  p{padding-left: 40px;padding-right: 40px;}
+</style>
+				<!-- footer -->
+				<div class="footer">
+					<div class="group-footer">
+						<div class="information">
+							<a href="#"><p class="text-align-left font-14">45Z, 371, Trapeang Chhuk, Teuk Thla, Sen Sok, Phnom Penh, Cambodia</p></a>
+							<a href="#" title=""><p>+855 23 44 00 70 / +855 12 44 00 70 / +855 10 44 00 70</p></a>
+							<a href="#" title=""><p>info@archsite.com | www.archsite.com</p></a>
+						</div>
+
+
+
+						<div class="dev-by">
+								<p>© 2018. All rights reserved. ARCHSITE, Sitemap</p>
+								<a href="http://tomnerbtech.com/#weprovide" target="_blank"><p>Developed by: tomnerbTech</p></a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			
+
+		</div>
+	</div>
+
+<!-- JS -->
+	<!-- <script src="//code.jquery.com/jquery-3.2.1.min.js"></script> -->
+	<script src="<?php echo $BATH_URL;?>assets/scripts/jquery.fancybox.min.js"></script>
+	
+</body>
+</html>
---
+++ mobile/content-home.php
@@ -0,0 +1,73 @@
+<?php 
+	// content home
+ ?>
+
+ <div id="wrapper">
+ 	<div id="content">
+ 		<div class="container-content">
+ 			<!-- baner home -->
+ 			<div class="banner-home padding-0 ">
+				<div class="group-banner-home " style="">
+					<div class="banner slick-main-banner position-relative"><!--banner-->
+
+						<div class="box-img-text position-relative" data-fullscreen>
+							<div class="box-img" style="">
+								<div class="gradientb"></div>
+								<img src="<?php echo $BATH_URL;?>assets/images/Architecture-homepage-Link-option21-banner-01.jpg">
+							</div>
+
+
+							<div class="description">
+								<h1>ARCHSITE FROM SKETCHES TO REAL LIFE</h1>
+		                    </div>
+						</div>
+
+						<div class="box-img-text position-relative" data-fullscreen>
+							<div class="box-img" style="">
+								<div class="gradientb"></div>
+								<img src="<?php echo $BATH_URL;?>assets/images/Architecture-homepage-Link-option21-banner-01.jpg">
+							</div>
+							<div class="description">
+								<h1>ARCHSITE FROM SKETCHES TO REAL LIFE</h1>
+		                    </div>
+						</div>
+
+						<div class="box-img-text position-relative" data-fullscreen>
+							<div class="box-img" style="">
+								<div class="gradientb"></div>
+								<img src="<?php echo $BATH_URL;?>assets/images/Architecture-homepage-Link-option21-banner-01.jpg">
+							</div>
+							<div class="description">
+								<h1>ARCHSITE FROM SKETCHES TO REAL LIFE</h1>
+		                    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+<style>
+#content .project{}
+#content .project .group-project{display: flex;justify-content: center;padding-top: 50px;}
+#content .project .title{display: flex;flex-direction: column;align-items: center;}
+#content .project .title h4{ margin: 0;font-size: 16px;text-transform: uppercase;letter-spacing: 7px;margin-top: 30px;}
+#content .project .title h2{margin: 0;font-size: 18px; margin-top: 50px;padding-right: 2%;}
+</style>
+			<!-- W E L C O M E T O A R C H S I T E -->
+			<div class="project">
+				<div class="group-project">
+					<div class="title">
+						<div class="linehigh"></div>
+						<h4>WELLCOME TO ARCHSITE</h4>
+						<h2>We build our projects with your dreams and ideas.</h2>
+					</div>
+				</div>
+			</div>
+			
+
+			
+			
+
+
+
+ 		</div>
+ 	</div>
+ </div>
